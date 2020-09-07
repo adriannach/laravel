ed8e071c8aa0c49dd22142015217e539352e219d
adrian-n_1996 <adrian-n_1996@wp.pl>
2020-09-07 09:08:21 +0200

    usuwanie komentarzy + passport

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::post('/login', 'Api\AuthController@login');
 
 Route::get('/comment', 'API\CommentController@index')->middleware('auth:api');
 Route::post('/comment/store', 'API\CommentController@store')->middleware('auth:api')->name('commentStore');
-
+Route::get('/comment/delete/{id}', 'API\CommentController@destroy')->middleware('auth:api');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
